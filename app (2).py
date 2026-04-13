8dea0e9db51e6f77f7debbcbde789d060a08b9f2
Senthoor-Mani <sm6252@srmist.edu.in>
2026-04-13 21:44:50 +0530

    commit

--- app (2).py
+++ app (2).py
@@ -6,7 +6,7 @@ st.title("🌾 Precision Crop Advisor")
 st.markdown("**99.5% XGBoost • Senthoor-Mani • SRM 2026**")
 
 N,P,K = st.slider("NPK",0,150,90),st.slider("P",0,100,42),st.slider("K",0,80,43)
-temp,hum,ph,rain = st.slider("Temp",10,40,21),st.slider("Humidity",20,100,82),st.slider("pH",0,14,6.5),st.slider("Rain",10,300,200)
+temp,hum,ph,rain = st.slider("Temp",10,40,21),st.slider("Humidity",20,100,82),st.slider("pH",0.0,14.0,6.5),st.slider("Rain",10,300,200)
 
 if st.button("🎯 PREDICT BEST CROP"):
     st.success("**RICE** - ₹85,000/acre profit!")
